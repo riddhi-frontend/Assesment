63f9f811e52616680d965ec2bdfa612175296993
PARMAR RIDDHI <148378854+riddhi-frontend@users.noreply.github.com>
2024-08-08 13:09:07 +0530

    Add files via upload

---
+++ Assesment/ASSESSMENT-4/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import RegistrationForm from './RegistrationForm';
+
+function App() {
+  return (
+    <div className="App">
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
